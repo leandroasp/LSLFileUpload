4ced465ce49c0a3439fb6c160b5a385ad4983b3d
Leandro Sales <leandroasp@gmail.com>
2012-08-05 22:56:15 -0300

    Upload and croping image

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,18 +16,36 @@ class IndexController extends Zend_Controller_Action
   {
     $form = new Application_Form_Example();
     if ($form->isValid($this->getRequest()->getPost())) {
+      $thumbnails = array(
+        array(
+          'append' => 'p',
+          'width'  => 40,
+          'height' => 60
+        ),
+        array(
+          'append' => 'g',
+          'width'  => 400,
+          'height' => 300
+        )
+      );
       $names = $this->getHelper('FileUpload')->getUploadNames(
         $form->uploader_count->getValue(),
         realpath(APPLICATION_PATH . "/../public/files"),
-        realpath(APPLICATION_PATH . "/../public/files/temp"));
-      
+        realpath(APPLICATION_PATH . "/../public/files/temp"),
+        $thumbnails);
+
+      echo '<pre>';
+      print_r($names);
+      echo '</pre>';
     } else {
       $this->view->form = $form;
       return $this->render('index');
     }
+    echo '<pre>';
     print_r($_REQUEST);
+    echo '</pre>';
   }
-  
+
   public function uploadAction()
   {
     $this->getHelper('layout')->disableLayout();
--- application/controllers/helpers/FileUpload.php
+++ application/controllers/helpers/FileUpload.php
@@ -8,12 +8,12 @@ class Zend_Controller_Action_Helper_FileUpload extends Zend_Controller_Action_He
     header("Cache-Control: no-store, no-cache, must-revalidate");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
-    
+
     array_merge(array(
       'targetDir' => APPLICATION_PATH,
       'cleanupTargetDir' => true,
       'maxFileAge' => 3600), $options);
-    
+
     $targetDir = $options['targetDir'];
     $cleanupTargetDir = $options['cleanupTargetDir']; // Remove old files
     $maxFileAge = $options['maxFileAge']; // Temp file age in seconds
@@ -120,16 +120,96 @@ class Zend_Controller_Action_Helper_FileUpload extends Zend_Controller_Action_He
     return '{"jsonrpc" : "2.0", "result" : null}';
   }
 
-  public function getUploadNames($size, $targetDir, $sourceDir)
+  public function getUploadNames($count, $targetDir, $sourceDir, $thumbnails = null)
   {
-    for($i = 0; $i < $size; $i++) {
-      if ($_REQUEST['uploader_' + $i + '_status'] != 'done') continue;
-      
-      $file = $_REQUEST['uploader_' + $i + '_tmpname'];
-      $id = preg_replace('/^(.+)\.[^\.]+$/','$1',$file);
-      $crop = $_REQUEST['crop_' + $id];
-      $caption = $_REQUEST['caption_' + $id];
-      //uploader_0_tmpname
+    $month = date("Ym");
+
+    if (!file_exists($targetDir . '/' . $month . '/')) {
+      mkdir($targetDir . '/' . $month . '/');
     }
+
+    $targetDir = $targetDir . '/' . $month;
+
+    if (is_null($thumbnails) || !is_array($thumbnails) || count($thumbnails) == 0) {
+      $thumbnails = array(array('append' => ''));
+    }
+
+    $names = array();
+    for($i = 0; $i < $count; $i++) {
+      if ($_REQUEST['uploader_' . $i . '_status'] != 'done') continue;
+
+      $file = $_REQUEST['uploader_' . $i . '_tmpname'];
+      $id = preg_replace('/^(.+)\.[^\.]+$/','$1',$file);
+      $ext = preg_replace('/^.+\.([^\.]+)$/','$1',$file);
+      $crop = $_REQUEST['crop_' . $id];
+      $caption = $_REQUEST['caption_' . $id];
+
+      $sourceFile = $sourceDir . '/' . $file;
+
+      list($w, $h, $type) = getimagesize($sourceFile);
+
+      $x1 = $y1 = $x2 = $y2 = '';
+      if ($crop != '') {
+        list($x1,$y1,$x2,$y2) = preg_split('/;/',$crop);
+      }
+      if (preg_match('/\D/',$x1)) $x1 = 0;
+      if (preg_match('/\D/',$y1)) $y1 = 0;
+      if (preg_match('/\D/',$x2)) $x2 = $w;
+      if (preg_match('/\D/',$y2)) $y2 = $h;
+
+      if ($type == 1) $sourceImg = imagecreatefromgif($sourceFile);
+      elseif ($type == 3) $sourceImg = imagecreatefrompng($sourceFile);
+      else $sourceImg = imagecreatefromjpeg($sourceFile);
+
+      $n = array(
+        'name' => $month . '/' . $id . '.' . $ext,
+        'caption' => $caption
+      );
+
+      foreach($thumbnails as $thumb) {
+        if (!isset($thumb['width']) || preg_match('/\D/',$thumb['width'])) $thumb['width'] = $x2 - $x1;
+        if (!isset($thumb['height']) || preg_match('/\D/',$thumb['height'])) $thumb['height'] = $y2 - $y1;
+
+        $sourceW = $x2 - $x1;
+        $sourceH = $y2 - $y1;
+
+        $sourceRatio = $sourceW/$sourceH;
+        $dstRatio = $thumb['width']/$thumb['height'];
+
+        $tempX1 = $x1;
+        $tempY1 = $y1;
+        if ($sourceRatio > $dstRatio) {
+          $tempW = round($sourceH * $dstRatio);
+          $tempX1 += round(($sourceW-$tempW)/2);
+          $sourceW = $tempW;
+        } else if($sourceRatio < $dstRatio) {
+          $tempH = round($sourceW / $dstRatio);
+          $tempY1 += round(($sourceH-$tempH)/2);
+          $sourceH = $tempH;
+        }
+
+        //echo "$tempX1 ; $tempY1 ; $x2 ; $y2 ; $sourceW ; $sourceH ; " . $thumb['width'] . ' ; ' . $thumb['height'] . '<br/>';
+
+        $targetImg = imagecreatetruecolor($thumb['width'], $thumb['height']);
+        imagecopyresampled($targetImg,$sourceImg,0,0,$tempX1,$tempY1,$thumb['width'], $thumb['height'],$sourceW,$sourceH);
+
+        if (!isset($thumb['append'])) $thumb['append'] = '';
+        $newName = $targetDir . '/' . $id . $thumb['append'] . '.' . $ext;
+
+        touch($newName);
+
+        if ($type == 1) imagegif($targetImg, $newName);
+        elseif ($type == 3) imagepng($targetImg, $newName);
+        else imagejpeg($targetImg, $newName, 90);
+
+        if ($thumb['append'] != '') {
+          $n['name_' . $thumb['append']] = $month . '/' . $id . $thumb['append'] . '.' . $ext;
+        }
+      }
+
+      array_push($names,$n);
+    }
+
+    return $names;
   }
 }
