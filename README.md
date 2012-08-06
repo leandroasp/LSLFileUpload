b8718ce83236a269647025eb35f7295148c23ff5
Leandro Sales <leandroasp@gmail.com>
2012-08-06 09:04:42 -0300

    Adding copyright

---
+++ README.md
@@ -0,0 +1,22 @@
+LSL FileUpload
+==============
+Is a multiple file upload with crop using Plupload, Jcrop, jQuery, jQuery UI, Twitter Bootstrap and Zend Framework.
+
+How to contribute
+-----------------
+Fork, code and submit an PULL REQUEST
+
+License
+-------
+LSL FileUpload is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LSL FileUpload is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LSL FileUpload.  If not, see <http://www.gnu.org/licenses/>.
--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * LSL FileUpload released under LGPL
+ * Author: Leandro Sales <leandroasp@gmail.com>
+ * https://github.com/leandroasp/LSLFileUpload
+ * Copyright (c) 2012
+ * -------------------------------------------------------------------
+ * This file is part of LSL FileUpload.
+ *
+ * LSL FileUpload is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LSL FileUpload is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LSL FileUpload.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
   public function _initHelpers()
